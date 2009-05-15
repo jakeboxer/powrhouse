b6dfc0739db36d5247d5c4b82bf3e4b805f99dbd
Jake Boxer <jacob.boxer@gmail.com>
2009-05-15 19:00:41 -0400

    Added two more useless prints.

--- chores/useless.py
+++ chores/useless.py
@@ -1,7 +1,11 @@
 import sys
 
+print("Oooh, a new branch (top)")
+
 print("Appears before, typed after.")
 
 print("Get rid of me.")
 
 print("Appears after, typed after.")
+
+print("Ooh, a new branch (bottom)")
