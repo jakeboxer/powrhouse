a02e5fb6de5376fd35d01cffa3db16ac88a86f7d
Jake Boxer <jacob.boxer@gmail.com>
2009-05-16 18:59:56 -0400

    Added get_hmate to hmates/__init__

---
+++ hmates/__init__.py
@@ -0,0 +1,19 @@
+
+def get_hmate(request):
+    from hmates.models import AnonymousHousemate
+    try:
+        # Try to get the user
+        user = request.user
+        
+        if user.is_anonymous():
+            # If the user is anonymous, the housemate should be too
+            hmate = AnonymousHousemate()
+        else:
+            # If the user isn't anonymous, get her housemate
+            from hmates.models import Housemate
+            hmate = Housemate.objects.get(user=user)
+    except KeyError:
+        # If there was no user, the housemate should be anonymous
+        hmate = AnonymousHousemate()
+    
+    return hmate
