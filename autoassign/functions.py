2e4e714ea99138cb8b8910c9940a289637cb31d1
Jake Boxer <jacob.boxer@gmail.com>
2009-05-18 23:07:54 -0400

    Moved chore auto-assignment unit test to autoassign app

---
+++ autoassign/functions.py
@@ -0,0 +1,4 @@
+from autoassign.classes import ChoreScheduler
+
+def get_chore_assignments (hhold):
+    return ChoreScheduler(hhold).get_assignments()
--- hholds/tests.py
+++ hholds/tests.py
@@ -47,29 +47,6 @@ class HouseholdTest (PowrTest):
         c2.get_newest_assignment().complete()
         num_unassigned_chores = len(hhold.get_chores_to_assign())
         self.failUnlessEqual(num_unassigned_chores, num_chores - 1)
-    
-    def _chore_in_assign_set (self, chore, assign_set):
-        for key in assign_set:
-            if chore in assign_set[key]: return True
-        
-        return False
-    
-    def test_get_assignments (self):
-        hhold  = self.hholds[0]
-        chores = hhold.chores.all()
-        
-        # get a chore that has a 1-day interval and a chore that has a longer
-        # interval
-        c1 = hhold.chores.filter(interval=1*SECS_PER_DAY)[0]
-        c2 = hhold.chores.filter(interval__gt=1*SECS_PER_DAY)[0]
-        
-        # At first, all the chores should be in the assignment set, since none
-        # have been done
-        assign_set = hhold.get_assignments()
-        for chore in chores:
-            self.failUnless(self._chore_in_assign_set(chore, assign_set))
-        
-        # After assigning a 1-day interval chore, it shouldn't be reassigned
         
         
 
