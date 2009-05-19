284e0a38ca23614f14132266ef60770da65c9da1
Jake Boxer <jacob.boxer@gmail.com>
2009-05-19 00:18:57 -0400

    Added function to retrieve households by hour

--- autoassign/functions.py
+++ autoassign/functions.py
@@ -1,4 +1,18 @@
+from hholds.models import Household
+from hholds.timezones import get_tzids_by_local_hour
 from autoassign.classes import ChoreScheduler
+import datetime
+
+def get_hholds_by_local_hr (hr, dt=datetime.datetime.utcnow()):
+    """
+    Returns all the households who locally (based on timezone) have the
+    specified hour.
+    
+    @param: hr Hour that the matching households should have
+    @param: dt Datetime to check on (defaults to now)
+    """
+    tzids = get_tzids_by_local_hour(hr, dt)
+    return Household.objects.filter(timezone_id__in=tzids)
 
 def get_chore_assignments (hhold):
     return ChoreScheduler(hhold).get_assignments()
