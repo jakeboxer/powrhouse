f43ff3cc7985cff0189f644fa43be59b676d595b
Jake Boxer <jacob.boxer@gmail.com>
2009-07-08 00:41:47 -0400

    Made add/edit chore pages work correctly, and made My Household page look right

--- chores/forms.py
+++ chores/forms.py
@@ -2,11 +2,19 @@ from chores.models import Chore, SECS_PER_DAY, DEFAULT_INTERVAL
 from chores.widgets import IntervalInput
 from django import forms
 from django.utils.translation import ugettext_lazy as _
-    
+
+INTERVAL_CHOICES = (
+    (1, _('Every day')),
+    (2, _('Every other day')),
+    (3, _('Every 3 days')),
+    (7, _('Every week')),
+    ('other', _('Other')),
+)
+
 class ChoreForm (forms.ModelForm):
     interval = forms.IntegerField(\
         label=_('How often should this chore be done?'), min_value=1,
-        max_value=365, widget=IntervalInput())
+        max_value=365, widget=forms.Select(choices=INTERVAL_CHOICES))
     
     class Meta:
         model   = Chore
@@ -17,11 +25,23 @@ class ChoreForm (forms.ModelForm):
         
         if "interval" in self.initial:
             # if there's already an initial value, turn it from seconds to days
-            self.initial["interval"] /= SECS_PER_DAY
+            # and record it as a separate attribute
+            days = self.initial["interval"] / SECS_PER_DAY
+            self.fields["interval"].widget.attrs["original"] = days
+            
+            # if the number of days is in the presets, set the initial value to
+            # the number of days. otherwise, set it to "other"
+            if days in [c[0] for c in INTERVAL_CHOICES if c[0] != "other"]:
+                self.initial["interval"] = days
+            else:
+                self.initial["interval"] = "other"
         else:
             # if there's no initial value yet, take the one that there's going
             # to be and turn it from seconds to days
             self.initial["interval"] = DEFAULT_INTERVAL / SECS_PER_DAY
+            
+            # there's no original value since it's new, so set it to blank
+            self.fields["interval"].widget.attrs["original"] = ""
     
     def clean_interval (self):
         return str(int(self.cleaned_data["interval"]) * SECS_PER_DAY)
--- media/js/base.js
+++ media/js/base.js
@@ -15,14 +15,24 @@ function setupHelpBubbles () {
 }
 
 function checkIntervalField () {
-    // If the interval field (on the New Chore page) is set to other, show the
-    // "other" text box. If it's not set to other, hide the "other" text box.
+    // If the interval field (on the New Chore page) is set to other, create the
+    // "other" box and manipulate the names so it gets submitted as "interval"
+    // instead of the select box
     if($("#id_interval").val() == "other"){
-        $("#id_interval_other").removeClass("hidden").addClass("inline")
-            .animate({backgroundColor: "#ffc"}, 500)
+        // find the original value and wipe it after saving it
+        original = $("#id_interval").attr("original");
+        $("#id_interval").attr("original", "");
+        
+        $("#id_interval").after('<div id="interval_other">Every <input type="text" name="interval" size="4" value="' + original + '"> day(s).</div>');
+        
+        $("#interval_other").animate({backgroundColor: "#ff6"}, 500)
             .animate({backgroundColor: "#fff"}, 500);
+        $("#id_interval").attr("name", "hidden_interval");
     }
     else{
-        $("#id_interval_other").removeClass("inline").addClass("hidden");
+        // remove the "other" box and swap back the field names so the select
+        // box gets submitted correctly
+        $("#interval_other").remove();
+        $("#id_interval").attr("name", "interval");
     }
 }
