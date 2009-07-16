7f0ebb423ea795c34d238f0c1233545a787d4433
Jake Boxer <jacob.boxer@gmail.com>
2009-07-16 17:32:17 -0400

    Made cron stuff work (supposedly) on Media Temple

--- cron.py
+++ cron.py
@@ -1,5 +1,10 @@
 # This should run every hour
 from autoassign.functions import *
+from django.contrib.contenttypes.models import ContentType
+from django.core.management import setup_environ
+import datetime, MySQLdb, re, settings, time
+
+setup_environ(settings)
 
 if __name__ == "__main__":
     send_morning_digests()
---
+++ cron.sh
@@ -0,0 +1,5 @@
+export PYTHONPATH=/home/71517/data/python/lib:/home/71517/data/python/bin:/home/71517/containers/django:/home/71517/data/python/django:/home/71517/containers/django/powr
+export DJANGO_SETTINGS_MODULE=settings
+export PYTHON_EGG_CACHE=/home/71517/containers/django/powr
+
+/usr/bin/python /home/71517/containers/django/powr/cron.py > /home/71517/containers/django/powr/cronlog
