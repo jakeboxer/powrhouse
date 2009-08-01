dbf1eeeb4e757e0c3711bda8639de47dc18061f2
Jake Boxer <jacob.boxer@gmail.com>
2009-08-01 13:49:43 -0400

    Added unicode method for top_notices models

--- top_notices/models.py
+++ top_notices/models.py
@@ -4,9 +4,16 @@ import datetime
 
 class TopNoticeSlug (models.Model):
     slug = models.SlugField(max_length=50, unique=True)
+    
+    def __unicode__ (self):
+        return self.slug
 
 class TopNoticeClosing (models.Model):
     top_notice = models.ForeignKey(TopNoticeSlug)
     hmate      = models.ForeignKey(Housemate)
     closed_at  = models.DateTimeField(blank=False, null=False,
         default=datetime.datetime.utcnow)
+    
+    def __unicode__ (self):
+        return "%s closed '%s'" \
+            % (self.hmate.get_full_name(), self.top_notice.slug)
