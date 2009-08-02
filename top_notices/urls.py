0f04f058ad97bb079157789096211449e171d498
Jake Boxer <jacob.boxer@gmail.com>
2009-08-02 15:55:45 -0400

    Fixed bug in top notice closing

--- top_notices/urls.py
+++ top_notices/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^close/(?P<slug>[-a-zA-Z0-9]+)/(?P<hmate_pk>[0-9]+)/$',
+    url(r'^close/(?P<slug>[-a-zA-Z0-9]+)/(?P<hmate_pk>\d+)/$',
         'top_notices.views.close', name="top_notice_close"),
 )
--- top_notices/views.py
+++ top_notices/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from hmates.models import Housemate
 from top_notices.models import TopNoticeSlug, TopNoticeClosing
 
-def close (slug, hmate_pk):
+def close (request, slug, hmate_pk):
     # Find the top notice and the housemate
     top_notice = get_object_or_404(TopNoticeSlug, slug=slug)
     hmate      = get_object_or_404(Housemate, pk=int(hmate_pk))
